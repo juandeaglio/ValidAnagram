2cdc074ee079bd2087681b4217ed59ddfd3ad879
Anthony Volkov <45726108+juandeaglio@users.noreply.github.com>
2023-10-25 15:40:09 -0400

    Fastest performance & best memory usage yet

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
@@ -9,28 +12,28 @@ class Solution:
         return len(s) == len(t) and equal
 
     def isCountEqualDictionary(self, s, t):
-        # uses a dictionary (which is just a hashmap) with O(1) lookup times
-        total_counts = {}
+        # uses a default dictionary, better than {} because it does not do a check for the key.
+        total_counts = defaultdict(int)
 
         for char in s:
-            total_counts[char] = total_counts.get(char, 0) + 1
+            total_counts[char] += 1
 
         for char in t:
-            if char not in total_counts:
+            if total_counts == 0:
                 return False
+            else:
+                total_counts[char] -= 1
 
-            total_counts[char] -= 1
-            if total_counts[char] == 0:
-                del total_counts[char]
-
-        return not total_counts
+        # make sure all values in total_counts == 0
+        return all(value == 0 for value in total_counts.values())
 
     def isCountEqualStrFunctions(self, s, t):
-        #more verbose and faster
+        # more verbose and faster for smaller strings because no dictionary is used.
         total_counts = 0
         original_length = len(s)
 
-        # Not sure on the Big O here, maybe O(n^2) worst case if we're dealing with a bunch of distinct characters, or all characters are distinct.
+        # Not sure on the Big O here, maybe O(n^2)
+        # worst case if we're dealing with a bunch of distinct characters, or all characters are distinct.
         while total_counts < original_length:
             letter = s[0]
             # O(n) for .count as well, since we're only checking a 1 length character in the words.
@@ -43,6 +46,5 @@ class Solution:
             # Seen in source: https://github.com/python/cpython/blob/main/Objects/unicodeobject.c#L10097
             s = s.replace(letter, "")
             total_counts += letter_count
-
 
         return True
